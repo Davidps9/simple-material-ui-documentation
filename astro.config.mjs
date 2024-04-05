b92cabccc1cbf96c5ce7e85385bcb65453593f06
DavidPS <david.pujol.santander@gmail.com>
2024-04-05 10:44:44 +0200

    starting SBUTTON guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-
-		starlight({
-			title: 'SMUL',
-			social: {
-				github: 'https://github.com/davidps9/ui-library',
-			},
-			sidebar: [
-				{
-					label: 'Quick Start',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Usage',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'SMUL',
+    social: {
+      github: 'https://github.com/davidps9/ui-library'
+    },
+    sidebar: [{
+      label: 'Quick Start',
+      autogenerate: {
+        directory: 'guides'
+      }
+    }, {
+      label: 'Usage',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), react(), tailwind()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'node_modules/simple-material-ui-library/**/*.{html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
